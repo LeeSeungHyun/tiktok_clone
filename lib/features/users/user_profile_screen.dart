c0f51348d3f8f4e1345f8aae7b342d093742c1f4
LeeSeungHyun <iop2480@naver.com>
2023-06-14 22:04:56 +0900

    SliverAppBar update

---
+++ lib/features/users/user_profile_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          snap: true,
+          floating: true, // 해당 아이템에서 스크롤 위로 올리자마자 바로 AppBar를 보이게 한다.
+          pinned: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
+            ],
+            background: Image.asset(
+              "assets/images/chester.png",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"),
+          ),
+        ),
+        SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 50,
+              (context, index) => Container(
+                color: Colors.amber[100 * (index % 9)],
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Item $index'),
+                ),
+              ),
+            ),
+            itemExtent: 100)
+      ],
+    );
+  }
+}
