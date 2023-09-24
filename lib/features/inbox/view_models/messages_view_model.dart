1949b3ba55a6faf41d8218c1900af547ae51e422
LeeSeungHyun <iop2480@naver.com>
2023-09-24 09:15:11 +0900

    inbox 소스 정리

--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/inbox/models/message.dart';
@@ -20,8 +21,9 @@ class MessagesViewModel extends AsyncNotifier<void> {
       final message = MessageModel(
         text: text,
         userId: user!.uid,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
       );
-      _repo.sendMessage(message);
+      await _repo.sendMessage(message);
     });
   }
 }
@@ -29,3 +31,24 @@ class MessagesViewModel extends AsyncNotifier<void> {
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
 );
+
+// autoDispose를 해주어야 화면에서 벗어났을 때 데이터를 listening 하지 않는다.
+final chatProvider = StreamProvider.autoDispose<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+
+  return db
+      .collection("chat_rooms")
+      .doc("qebrazcxNMLuhs2mXMKU")
+      .collection("texts")
+      .orderBy("createdAt")
+      .snapshots()
+      .map(
+        (event) => event.docs
+            .map(
+              (doc) => MessageModel.fromJson(
+                doc.data(),
+              ),
+            )
+            .toList(),
+      );
+});
